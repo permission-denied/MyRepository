537878580cec93575e6280ee14f28bb651c1ca14
limei <limei@dae.org>
2019-03-26 15:54:52 +0800

    fourteen

--- MayBaseProject/app/src/main/java/com/may/base/Constants.java
+++ MayBaseProject/app/src/main/java/com/may/base/Constants.java
@@ -19,6 +19,7 @@ public class Constants {
     public static String LOG_TAG_NAME_ELVEN= "etcgame";
     public static String LOG_TAG_NAME_TWELVE= "etcgame";
     public static String LOG_TAG_NAME_THIRTEEN= "etcgame";
+    public static String LOG_TAG_NAME_FOURTEEN= "etcgame";
 
 
     public static class IntentExtraKey {
