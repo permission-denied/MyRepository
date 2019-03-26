5f16210301195e519377bc365e772d12adbb1b88
limei <limei@dae.org>
2019-03-26 11:30:38 +0800

    add  four

--- MayBaseProject/app/src/main/java/com/may/base/Constants.java
+++ MayBaseProject/app/src/main/java/com/may/base/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
     public static String LOG_TAG_NAME = "etcgame";
     public static String LOG_TAG_NAME_TWO = "etcgame";
     public static String LOG_TAG_NAME_Three = "etcgame";
+    public static String LOG_TAG_NAME_FOUR = "etcgame";
 
     public static class IntentExtraKey {
 
